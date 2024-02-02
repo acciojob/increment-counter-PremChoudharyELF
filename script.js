0c86a3571e8db62c960bb2304adefb2633c21d7c
PremChoudharyELF <123535773+PremChoudharyELF@users.noreply.github.com>
2024-02-02 10:35:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 let count=0;
 document.getElementById("btn").onclick=function(){
 	count+=1;
+	document.getElementById("counter").innerHTML=count;
+	alert(count);
 }
-document.getElementById("counter").innerHTML=count;
